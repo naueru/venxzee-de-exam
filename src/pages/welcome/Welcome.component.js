ea19dfd6199edc9d2dcf2f38e7b55478afcfa6f7
naueru <adolfonahuel@gmail.com>
2021-09-13 16:36:20 -0300

    Feature/modal (#1)
    * Creates common components folder and namespace
    
    * Creates base modal component
    
    * Updates modal component and styles. Implements modal in welcome page
    
    Co-authored-by: Nahuel Andrada <Ahuel.Andrada@elliemae.com>

--- src/pages/welcome/Welcome.component.js
+++ src/pages/welcome/Welcome.component.js
@@ -1,13 +1,16 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx } from '@emotion/react';
+import { useState } from 'react';
 
 import { loginService } from 'services/rest';
+
+import Modal from 'components/modal';
 
 import styles from './styles';
 
 const Welcome = () => {
-
+  const [ showLoginModal, setShowLoginModal ] = useState(false);
   const login = async (data) => {
     return await loginService(data)
       .then((res) => console.log('ToDo: store ->', res?.token))
@@ -15,8 +18,14 @@ const Welcome = () => {
 
   return <div css={styles.container}>
     <header css={styles.header}>
-      <button css={styles.button} onClick={() => login({})}>Login</button>
+      <button css={styles.button} onClick={() => setShowLoginModal(true)}>Login</button>
     </header>
+    {showLoginModal && <Modal
+      title={'Login'}
+      onClose={() => setShowLoginModal(false)}
+    >
+      Login form
+    </Modal>}
   </div>;
 };
 
