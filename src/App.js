b2ad0de165766d3ce0888638f7d167c5656ca60d
Nahuel Andrada <Ahuel.Andrada@elliemae.com>
2021-09-13 18:59:24 -0300

    ReplayList - Adds new route for posts

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Switch, Route } from 'react-router';
 import './App.css';
 
 import Welcome from 'pages/welcome';
+import PostPage from 'pages/postPage';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" render={() => <Welcome />} />
+        <Route path="/post/:id" render={() => <PostPage />} />
       </Switch>
     </div>
   );
