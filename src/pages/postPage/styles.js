207204e980080bfccdd9a51cd0ec06a358cd6e7c
naueru <adolfonahuel@gmail.com>
2021-09-13 19:02:30 -0300

    Feature/replays list (#6)
    * ReplaysList - Creates basic post page
    
    * ReplayList - Adds new route for posts
    
    * ReplayList - Implements parser and fixes missing prop
    
    * ReplayList - Updates post page component and styles
    
    * ReplayList - Implements single post fetching service
    
    Co-authored-by: Nahuel Andrada <Ahuel.Andrada@elliemae.com>

---
+++ src/pages/postPage/styles.js
@@ -0,0 +1,20 @@
+import { css } from '@emotion/react';
+
+const container = css`
+  display: flex;
+  justify-content: center;
+  max-height: 100vh;
+  max-width: 100vw;
+  padding: 20px;
+`;
+
+const contentContainer = css`
+  max-width: 80vw;
+`;
+
+const styles = {
+  container,
+  contentContainer
+};
+
+export default styles;
