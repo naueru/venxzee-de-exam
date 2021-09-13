5fc36ad3ee4eacf2fed6750935d960bf55798154
Nahuel Andrada <Ahuel.Andrada@elliemae.com>
2021-09-13 16:41:53 -0300

    Creates basic login form component

---
+++ src/components/sessionForms/login/Login.component.js
@@ -0,0 +1,19 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from '@emotion/react';
+
+import PropTypes from 'prop-types';
+
+import styles from '../styles';
+
+const Login = () => {
+  return (
+    <form css={styles.container}>
+    </form>
+)};
+
+Login.propTypes = {};
+
+Login.defaultProps = {};
+
+export default Login;
