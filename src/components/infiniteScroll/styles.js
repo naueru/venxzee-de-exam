ab5a471e79c0a5509a35a9faaee166289a8286b5
naueru <adolfonahuel@gmail.com>
2021-09-13 20:00:10 -0300

    Feature/infinite scrolling (#8)
    * InfiniteScrolling - Adds lodash dependency and hooks namespace
    
    * InfiniteScrolling - Implements infinite scrolling for posts
    
    * InfiniteScrolling - Creates folder for custom hooks
    
    * InfiniteScrolling - Creates infiniteScrolling related components
    
    Co-authored-by: Nahuel Andrada <Ahuel.Andrada@elliemae.com>

---
+++ src/components/infiniteScroll/styles.js
@@ -0,0 +1,16 @@
+import { css } from '@emotion/react';
+
+const wrapper = css`
+  background-attachment: fixed;
+`;
+
+const bottom = css`
+  text-align: center;
+`;
+
+const styles = {
+  wrapper,
+  bottom,
+};
+
+export default styles;
