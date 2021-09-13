8e0a2207388efef0a11cb1adcd50098661f2b1c3
Nahuel Andrada <Ahuel.Andrada@elliemae.com>
2021-09-13 19:59:31 -0300

    InfiniteScrolling - Creates infiniteScrolling related components

---
+++ src/components/infinitePostList/styles.js
@@ -0,0 +1,18 @@
+import { css } from '@emotion/react';
+
+const empty = css`
+  font-size: 32px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+const bottomWrapper = css`
+  margin-bottom: 30px;
+`;
+
+const styles = {
+  empty,
+  bottomWrapper
+};
+
+export default styles;
