b0c9cb7b8b124d7b2f55eda7218b8a62a7a7e71e
Nahuel Andrada <Ahuel.Andrada@elliemae.com>
2021-09-13 17:04:25 -0300

    Login - Implements login component

--- src/pages/welcome/Welcome.component.js
+++ src/pages/welcome/Welcome.component.js
@@ -6,14 +6,17 @@ import { useState } from 'react';
 import { loginService } from 'services/rest';
 
 import Modal from 'components/modal';
+import Login from 'components/sessionForms/login';
 
 import styles from './styles';
 
 const Welcome = () => {
   const [ showLoginModal, setShowLoginModal ] = useState(false);
+  const [ loginError, setLoginError ] = useState('');
   const login = async (data) => {
     return await loginService(data)
       .then((res) => console.log('ToDo: store ->', res?.token))
+      .catch(err => setLoginError(err.error));
   };
 
   return <div css={styles.container}>
@@ -24,7 +27,7 @@ const Welcome = () => {
       title={'Login'}
       onClose={() => setShowLoginModal(false)}
     >
-      Login form
+      <Login onSubmit={login} errorMsg={loginError} />
     </Modal>}
   </div>;
 };
