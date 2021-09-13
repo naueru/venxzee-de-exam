93b1ba27ac4dcd8310c997fdb9fc8770eb3e77a2
Nahuel Andrada <Ahuel.Andrada@elliemae.com>
2021-09-13 16:15:45 -0300

    Implements basic login button and api call

--- src/pages/welcome/Welcome.component.js
+++ src/pages/welcome/Welcome.component.js
@@ -2,11 +2,22 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/react';
 
+import { loginService } from 'services/rest';
+
 import styles from './styles';
 
 const Welcome = () => {
 
-  return <div css={styles.container}>Welcome</div>;
+  const login = async (data) => {
+    return await loginService(data)
+      .then((res) => console.log('ToDo: store ->', res?.token))
+  };
+
+  return <div css={styles.container}>
+    <header css={styles.header}>
+      <button css={styles.button} onClick={() => login({})}>Login</button>
+    </header>
+  </div>;
 };
 
 export default Welcome;
