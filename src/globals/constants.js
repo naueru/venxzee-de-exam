9eccf41d16b395bbdd6039d9c77ebcf7fdc6f4ae
Nahuel Andrada <Ahuel.Andrada@elliemae.com>
2021-09-13 17:57:50 -0300

    Post - Adds lorem ipsum constant

--- src/globals/constants.js
+++ src/globals/constants.js
@@ -17,3 +17,6 @@ export const MESSAGES = {
   PASSWORD_MISSMATCH: 'Password and repeat password should match',
   MISSING_PASSWORD: 'Please type the password'
 };
+
+export const LOREM_IPSUM = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute';
+
