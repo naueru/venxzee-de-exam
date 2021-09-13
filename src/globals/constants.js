3081375c49674efaf0de1c1c0557319172e91045
naueru <adolfonahuel@gmail.com>
2021-09-13 18:12:55 -0300

    Feature/post (#4)
    * Post - Creates basic post component
    
    * Post - Adds lorem ipsum constant
    
    * Post - Creates parser for response data
    
    * Post - Adds moocked response for testing
    
    * Post - Updates post card component and styles
    
    * Post - Implements post card component
    
    Co-authored-by: Nahuel Andrada <Ahuel.Andrada@elliemae.com>

--- src/globals/constants.js
+++ src/globals/constants.js
@@ -17,3 +17,52 @@ export const MESSAGES = {
   PASSWORD_MISSMATCH: 'Password and repeat password should match',
   MISSING_PASSWORD: 'Please type the password'
 };
+
+export const LOREM_IPSUM = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute';
+
+export const MOCKS = {
+  UNKNOWN: [
+    {
+        "id": 1,
+        "name": "cerulean",
+        "year": 2000,
+        "color": "#98B2D1",
+        "pantone_value": "15-4020"
+    },
+    {
+        "id": 2,
+        "name": "fuchsia rose",
+        "year": 2001,
+        "color": "#C74375",
+        "pantone_value": "17-2031"
+    },
+    {
+        "id": 3,
+        "name": "true red",
+        "year": 2002,
+        "color": "#BF1932",
+        "pantone_value": "19-1664"
+    },
+    {
+        "id": 4,
+        "name": "aqua sky",
+        "year": 2003,
+        "color": "#7BC4C4",
+        "pantone_value": "14-4811"
+    },
+    {
+        "id": 5,
+        "name": "tigerlily",
+        "year": 2004,
+        "color": "#E2583E",
+        "pantone_value": "17-1456"
+    },
+    {
+        "id": 6,
+        "name": "blue turquoise",
+        "year": 2005,
+        "color": "#53B0AE",
+        "pantone_value": "15-5217"
+    }
+  ]
+};
