3e20db3079c1e5ff1f15dcbc0e2588ba3e37fe6b
Nahuel Andrada <Ahuel.Andrada@elliemae.com>
2021-09-13 17:53:38 -0300

    Post - Creates basic post component

---
+++ src/components/postCard/PostCard.component.js
@@ -0,0 +1,16 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from '@emotion/react';
+
+import styles from './styles';
+
+const PostCard = () => {
+  return (
+    <article css={styles.container}>PostCard</article>);
+};
+
+PostCard.propTypes = {};
+
+PostCard.defaultProps = {};
+
+export default PostCard;
