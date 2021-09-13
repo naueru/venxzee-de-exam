5b59e54ed3af3ec477630899969ee1aa42a9ca20
Nahuel Andrada <Ahuel.Andrada@elliemae.com>
2021-09-13 16:25:00 -0300

    Creates base modal component

---
+++ src/components/modal/Modal.component.js
@@ -0,0 +1,16 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from '@emotion/react';
+import PropTypes from 'prop-types';
+
+import styles from './styles';
+
+const Modal = () => {
+  return <div css={styles.container}>Modal</div>;
+};
+
+Modal.propTypes = {};
+
+Modal.defaultProps = {};
+
+export default Modal;
