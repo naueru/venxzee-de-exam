9b54f155bdad2230800ddd3b151062b09b21f699
Nahuel Andrada <Ahuel.Andrada@elliemae.com>
2021-09-13 16:07:38 -0300

    Adds some basic request related constants

---
+++ src/globals/constants.js
@@ -0,0 +1,13 @@
+export const BASE_API_URL = 'https://reqres.in/api/';
+
+export const API_URLS = {
+  USERS: BASE_API_URL + 'users',
+  POSTS: BASE_API_URL + 'posts',
+  REGISTER: BASE_API_URL + 'register',
+  LOGIN: BASE_API_URL + 'login'
+};
+
+export const METHODS = {
+  GET: 'GET',
+  POST: 'POST'
+};
