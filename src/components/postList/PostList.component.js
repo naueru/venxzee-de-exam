408cf5ee578a4dc83e3b61a763f659dd17ae590d
Nahuel Andrada <Ahuel.Andrada@elliemae.com>
2021-09-13 18:17:32 -0300

    PostList - Creates basic list component

---
+++ src/components/postList/PostList.component.js
@@ -0,0 +1,17 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from '@emotion/react';
+
+import PropTypes from 'prop-types';
+
+import styles from './styles';
+
+const PostList = () => {
+  return <div css={styles.container}>List</div>
+};
+
+PostList.propTypes = {};
+
+PostList.defaultProps = {};
+
+export default PostList;
---
+++ src/components/postList/styles.js
@@ -0,0 +1,10 @@
+import { css } from '@emotion/react';
+
+const container = css``;
+
+
+const styles = {
+  container
+};
+
+export default styles;
