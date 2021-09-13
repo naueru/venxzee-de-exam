2b745c6ceb3a5446666dc3c20091e94819f79d8a
Nahuel Andrada <Ahuel.Andrada@elliemae.com>
2021-09-13 15:52:34 -0300

    Creates base welcome page component

---
+++ src/pages/welcome/Welcome.component.js
@@ -0,0 +1,12 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from '@emotion/react';
+
+import styles from './styles';
+
+const Welcome = () => {
+
+  return <div css={styles.container}>Welcome</div>;
+};
+
+export default Welcome;
