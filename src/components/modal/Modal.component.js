7d1bc8c14667694c1a0d042fde891268a556273f
Nahuel Andrada <Ahuel.Andrada@elliemae.com>
2021-09-13 16:34:43 -0300

    Updates modal component and styles. Implements modal in welcome page

--- src/components/modal/Modal.component.js
+++ src/components/modal/Modal.component.js
@@ -5,12 +5,30 @@ import PropTypes from 'prop-types';
 
 import styles from './styles';
 
-const Modal = () => {
-  return <div css={styles.container}>Modal</div>;
+const Modal = ({ children, onClose, title }) => {
+  return (
+    <div css={styles.curtain}>
+      <div css={styles.modalContainer}>
+        <header css={styles.modalHeader}>
+          <h2 css={styles.modalTitle}>{title}</h2>
+          <button css={styles.modalClose} onClick={onClose}>x</button>
+        </header>
+        <div css={styles.modalContentContainer}>
+          {children}
+        </div>
+      </div>
+    </div>);
 };
 
-Modal.propTypes = {};
+Modal.propTypes = {
+  children: PropTypes.any.isRequired,
+  onClose: PropTypes.func,
+  title: PropTypes.string
+};
 
-Modal.defaultProps = {};
+Modal.defaultProps = {
+  onClose: () => {},
+  title: ''
+};
 
 export default Modal;
