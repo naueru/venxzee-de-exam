01bd29db97723981b87bc8f00446e39cec0de1fd
Nahuel Andrada <Ahuel.Andrada@elliemae.com>
2021-09-13 18:42:16 -0300

    ReplaysList - Creates basic post page

---
+++ src/pages/postPage/PostPage.component.js
@@ -0,0 +1,11 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from '@emotion/react';
+
+import styles from './styles';
+
+const PostPage = () => {
+  return <div css={styles.container}>Post</div>
+};
+
+export default PostPage;
---
+++ src/pages/postPage/styles.js
@@ -0,0 +1,15 @@
+import { css } from '@emotion/react';
+
+const container = css`
+  display: flex;
+  justify-content: center;
+  max-height: 100vh;
+  max-width: 100vw;
+  padding: 20px;
+`;
+
+const styles = {
+  container
+};
+
+export default styles;
